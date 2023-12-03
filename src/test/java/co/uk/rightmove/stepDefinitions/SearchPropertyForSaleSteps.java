7f151416418f0cefc0e86e86f2bd7ad0c0b532ad
Mauyon2023 <ezekielsohe@gmail.com>
2023-12-03 00:06:09 +0000

    first commit

---
+++ src/test/java/co/uk/rightmove/stepDefinitions/SearchPropertyForSaleSteps.java
@@ -0,0 +1,88 @@
+package co.uk.rightmove.stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class SearchPropertyForSaleSteps {
+    @Given("I navigate to the homepage")
+    public void i_navigate_to_the_homepage() {
+
+    }
+
+    @When("I enter {string} in the search field")
+    public void i_enter_in_the_search_field(String string) {
+
+    }
+
+    @When("I click on For sale button")
+    public void i_click_on_for_sale_button() {
+
+    }
+
+    @When("I ensure that the search criteria page is displayed")
+    public void i_ensure_that_the_search_criteria_page_is_displayed() {
+
+    }
+
+    @When("I select radius {string} of the location")
+    public void i_select_radius_of_the_location(String string) {
+
+    }
+
+    @When("I select minimun price of {string}")
+    public void i_select_minimun_price_of(String string) {
+
+    }
+
+    @When("I select maximum price of {string}")
+    public void i_select_maximum_price_of(String string) {
+
+    }
+
+    @When("I select minimum number of bedrooms as {string}")
+    public void i_select_minimum_number_of_bedrooms_as(String string) {
+
+    }
+
+    @When("I select maximum number of bedrooms as {string}")
+    public void i_select_maximum_number_of_bedrooms_as(String string) {
+
+    }
+
+    @When("I select {string} as property type")
+    public void i_select_as_property_type(String string) {
+
+    }
+
+    @When("I select {string} as added to site option")
+    public void i_select_as_added_to_site_option(String string) {
+
+    }
+
+    @When("I click on find property")
+    public void i_click_on_find_property() {
+
+    }
+
+    @When("I verify that the search result page is displayed")
+    public void i_verify_that_the_search_result_page_is_displayed() {
+
+    }
+
+    @When("I click on second result")
+    public void i_click_on_second_result() {
+
+    }
+
+    @Then("the details of the selected property are displayed")
+    public void the_details_of_the_selected_property_are_displayed() {
+
+    }
+
+
+    @And("I click on {string} button")
+    public void iClickOnButton(String arg0) {
+    }
+}
